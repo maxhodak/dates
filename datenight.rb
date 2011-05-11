acb5f29ee1c5645145e407c3091c08bd4d630677
Daphne Ezer <daphne.ezer@gmail.com>
2011-05-11 17:50:00 -0400

    covar matrix and choose

--- datenight.rb
+++ datenight.rb
@@ -9,13 +9,13 @@ class DateIdeas
   def initialize()
     @date_ideas = []
   end
-
+# adds a date to the date_ideas list
   def date(name, &block)
     date = DateIdea.new(name)
     date.instance_eval(&block)
     @date_ideas << date
   end
-
+#prints out date ideas
   def to_s
     str = "Date ideas:\n"
     @date_ideas.each do |f|
@@ -23,30 +23,71 @@ class DateIdeas
     end
     str
   end
-
+#TODO: modify method to produce co-variance 
   def outcome_covariance
     cov = {}
-    ATTRIBUTES.each do |attribute|
-      cov[attribute] = 0
-      puts complete.collect { |idea|
-        [idea.attribute(attribute), idea.score]
-      }
-    end
-  end
 
+    @date_ideas.each do |idea_a|
+	    @date_ideas.each do |idea_b|
+		    cov[[idea_a, idea_b]]=0
+		    length_a=0
+		    length_b=0
+		    all_keys=idea_a.attributes.keys
+		    all_keys.each do |attribute|
+		   	cov[[idea_a, idea_b]]=cov[[idea_a, idea_b]]+idea_a.attributes[attribute]*idea_b.attributes[attribute]
+			length_a=length_a+idea_a.attributes[attribute]**2
+			length_b=length_b+idea_b.attributes[attribute]**2
+		    end
+		    
+		
+		    cov[[idea_a, idea_b]]=(cov[[idea_a, idea_b]]/(Math.sqrt(length_a)))/Math.sqrt(length_b)
+		   # puts cov[[idea_a, idea_b]]
+
+	    end
+    end
+
+    return cov
+  end
+#is complete?
   def complete
     @date_ideas.reject { |idea| idea.complete? }
   end
-
+#is incomplete?
   def incomplete
     @date_ideas.reject { |idea| !idea.complete? }
   end
+#randomly selects date (TODO: weight)
+  def choose(weather, time)
+	  #A lot of what i did here was completely arbitrary, feel free to modify
+cov=outcome_covariance()
+	  prior_weight=0.5
+	  sum=0.0
+	  
+	  date_scores={}
 
-  def choose
-    num_dates = incomplete.length
-    incomplete[rand(num_dates)]
-  end
+	incomplete.each do |option|
+		if option.requirements[:"weather"]<weather && option.requirements[:"hours"]<=time  #refactor
+		complete.each do |point|
+			value= 1.1**((point.score-5)*cov[[option, point]]*prior_weight) #GENERALIZE
+			date_scores[option]=value
+			sum+=value
+		end
+		end
+	end
 
+	rand_choice=rand()*sum
+#	puts rand_choice
+#	puts sum
+	incomplete.each do |option|
+		if option.requirements[:"weather"]<weather && option.requirements[:"hours"]<=time
+		rand_choice-=date_scores[option]
+		if(rand_choice<=0)
+			return option
+		end
+		end
+	end
+end
+#makes a new dateIdeas object and adds the plan?
   def self.plan(&block)
     $dates = DateIdeas.new
     $dates.instance_eval(&block)
@@ -54,12 +95,13 @@ class DateIdeas
 
 end
 
+#a particular date idea
 class DateIdea
 
   def initialize(name = "")
     @name = name.to_s
     @attributes = {}
-    @requires = {}
+    @requirements = {}
     @outcome = nil
   end
 
@@ -71,7 +113,7 @@ class DateIdea
 
   def requires(args)
     args.each do |field, value|
-      @requires[field] = value
+      @requirements[field] = value
     end
   end
 
@@ -87,6 +129,14 @@ class DateIdea
     @attributes[name]
   end
   
+def requirements
+	@requirements
+end
+
+def name
+	@name
+end
+
   def score
     @outcome
   end
@@ -109,14 +159,36 @@ end
 infile = ARGV[0]
 require infile
 
-if ARGV.length == 1
-  $dates.outcome_covariance
-else
-  command = ARGV[1]
-  
-  if command == 'list'
-    puts $dates
-  else
-    puts "Usage: ./datenight.rb <ideas-file> [list]"
-  end
-end
+weather=10
+time=24
+i=1
+while i<ARGV.length do
+        command =ARGV[i]
+	if command == 'list'
+		puts $dates
+	elsif command == 'weather'
+		i=i+1
+		weather=ARGV[i].to_f
+	elsif command == 'time'
+		i=i+1
+		time=ARGV[i].to_f
+	else
+                 puts "Usage: ./datenight.rb <ideas-file> [weather float] [time float] [list]"
+        end
+	i=i+1
+end	
+
+    date_map={}	
+	for i in 0 ..10
+		temp=$dates.choose(weather, time)
+		if(!date_map.has_key?(temp))
+			date_map[temp]=0
+		end
+     		date_map[temp]=date_map[temp]+1
+	end
+		date_map.keys.each do |key_val|
+		     puts key_val.name
+		    puts date_map[key_val] 
+		end
+
+#end
--- ideas.rb
+++ ideas.rb
@@ -3,9 +3,19 @@ DateIdeas.plan do
   date "watch primer in north" do
     kind :adventure => 2, :romantic => 4, :intellectual => 6, :athletic => 0
     requires :weather => 0, :hours => 2
-    outcome 6
+    outcome -100
   end
-  
+ 
+date "very close to primer" do
+    kind :adventure => 2, :romantic => 4, :intellectual => 6, :athletic => 1
+    requires :weather => 0, :hours => 2
+  end
+
+date "very far from primer" do
+    kind :adventure => 10, :romantic => 10, :intellectual => 0, :athletic => 10
+    requires :weather => 0, :hours => 2
+  end
+
   date "escher in the DiVE" do
     kind :adventure => 4, :romantic => 3, :intellectual => 4, :athletic => 0
     requires :weather => 0, :hours => 2
@@ -17,7 +27,7 @@ DateIdeas.plan do
   end
   
   date "duke observatory" do
-    kind :adventure => 6, :romantic => 7, :intellectual => 5, :athletic => 1
+    kind :adventure => 100, :romantic => 7, :intellectual => 5, :athletic => 1
     requires :weather => 8, :hours => 4
   end
   
