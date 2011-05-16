640de46770a33e26b37ff0ab188688a3396a5253
Max Hodak <maxhodak@gmail.com>
2011-05-16 18:51:31 -0400

    code clean-up; moved cli to optparse and other refactoring.  the selection code probably doesnt do what you think it does ;)

--- datenight.rb
+++ datenight.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'optparse'
+
 $dates = nil
 
 class DateIdeas
@@ -9,13 +11,15 @@ class DateIdeas
   def initialize()
     @date_ideas = []
   end
-# adds a date to the date_ideas list
+
+  # adds a date to the date_ideas list
   def date(name, &block)
     date = DateIdea.new(name)
     date.instance_eval(&block)
     @date_ideas << date
   end
-#prints out date ideas
+
+  #prints out date ideas
   def to_s
     str = "Date ideas:\n"
     @date_ideas.each do |f|
@@ -23,71 +27,72 @@ class DateIdeas
     end
     str
   end
-#TODO: modify method to produce co-variance 
+
+  #TODO: modify method to produce co-variance 
   def outcome_covariance
     cov = {}
 
     @date_ideas.each do |idea_a|
-	    @date_ideas.each do |idea_b|
-		    cov[[idea_a, idea_b]]=0
-		    length_a=0
-		    length_b=0
-		    all_keys=idea_a.attributes.keys
-		    all_keys.each do |attribute|
-		   	cov[[idea_a, idea_b]]=cov[[idea_a, idea_b]]+idea_a.attributes[attribute]*idea_b.attributes[attribute]
-			length_a=length_a+idea_a.attributes[attribute]**2
-			length_b=length_b+idea_b.attributes[attribute]**2
-		    end
-		    
-		
-		    cov[[idea_a, idea_b]]=(cov[[idea_a, idea_b]]/(Math.sqrt(length_a)))/Math.sqrt(length_b)
-		   # puts cov[[idea_a, idea_b]]
+      @date_ideas.each do |idea_b|
+        cov[[idea_a, idea_b]]=0
+        length_a=0
+        length_b=0
+        all_keys=idea_a.attributes.keys
+        all_keys.each do |attribute|
+          cov[[idea_a, idea_b]]=cov[[idea_a, idea_b]]+idea_a.attributes[attribute]*idea_b.attributes[attribute]
+          length_a=length_a+idea_a.attributes[attribute]**2
+          length_b=length_b+idea_b.attributes[attribute]**2
+        end
 
-	    end
+        cov[[idea_a, idea_b]]=(cov[[idea_a, idea_b]]/(Math.sqrt(length_a)))/Math.sqrt(length_b)
+        # puts cov[[idea_a, idea_b]]
+
+      end
     end
 
-    return cov
+    cov
   end
-#is complete?
+
   def complete
     @date_ideas.reject { |idea| idea.complete? }
   end
-#is incomplete?
+
   def incomplete
     @date_ideas.reject { |idea| !idea.complete? }
   end
-#randomly selects date (TODO: weight)
+
+  #randomly selects date (TODO: weight)
   def choose(weather, time)
-	  #A lot of what i did here was completely arbitrary, feel free to modify
-cov=outcome_covariance()
-	  prior_weight=0.5
-	  sum=0.0
-	  
-	  date_scores={}
 
-	incomplete.each do |option|
-		if option.requirements[:"weather"]<weather && option.requirements[:"hours"]<=time  #refactor
-		complete.each do |point|
-			value= 1.1**((point.score-5)*cov[[option, point]]*prior_weight) #GENERALIZE
-			date_scores[option]=value
-			sum+=value
-		end
-		end
-	end
+    cov = outcome_covariance()
+    prior_weight = 0.5
+    sum = 0.0
 
-	rand_choice=rand()*sum
-#	puts rand_choice
-#	puts sum
-	incomplete.each do |option|
-		if option.requirements[:"weather"]<weather && option.requirements[:"hours"]<=time
-		rand_choice-=date_scores[option]
-		if(rand_choice<=0)
-			return option
-		end
-		end
-	end
-end
-#makes a new dateIdeas object and adds the plan?
+    date_scores = {}
+
+    incomplete.each do |option|
+      if option.requirements[:weather] < weather && option.requirements[:hours] <= time  #refactor
+        complete.each do |point|
+          value = 1.1**((point.score-5)*cov[[option, point]]*prior_weight) #GENERALIZE
+          date_scores[option] = value
+          sum += value
+        end
+      end
+    end
+
+    rand_choice=rand()*sum
+    #  puts rand_choice
+    #  puts sum
+    incomplete.each do |option|
+      if option.requirements[:weather] < weather && option.requirements[:hours] <= time
+        rand_choice -= date_scores[option]
+        if(rand_choice <= 0)
+          return option # this probably doesn't do what you think it does.
+        end
+      end
+    end
+  end
+
   def self.plan(&block)
     $dates = DateIdeas.new
     $dates.instance_eval(&block)
@@ -120,7 +125,7 @@ class DateIdea
   def outcome(score)
     @outcome = score
   end
-  
+
   def attributes
     @attributes
   end
@@ -128,14 +133,14 @@ class DateIdea
   def attribute(name)
     @attributes[name]
   end
-  
-def requirements
-	@requirements
-end
 
-def name
-	@name
-end
+  def requirements
+    @requirements
+  end
+
+  def name
+    @name
+  end
 
   def score
     @outcome
@@ -147,7 +152,7 @@ end
 
   def to_s
     attr_str = @attributes.map { |k,v|
-      "#{k}: #{v}"
+    "#{k}: #{v}"
     }.join(", ")
     date_str = "#{@name}: (#{attr_str})"
     date_str = date_str + ", outcome: #{@outcome}" unless @outcome == nil
@@ -156,39 +161,39 @@ end
 
 end
 
-infile = ARGV[0]
-require infile
+command = "choose"
+options = {}
 
-weather=10
-time=24
-i=1
-while i<ARGV.length do
-        command =ARGV[i]
-	if command == 'list'
-		puts $dates
-	elsif command == 'weather'
-		i=i+1
-		weather=ARGV[i].to_f
-	elsif command == 'time'
-		i=i+1
-		time=ARGV[i].to_f
-	else
-                 puts "Usage: ./datenight.rb <ideas-file> [weather float] [time float] [list]"
-        end
-	i=i+1
-end	
+optparse = OptionParser.new do |opts|
 
-    date_map={}	
-	for i in 0 ..10
-		temp=$dates.choose(weather, time)
-		if(!date_map.has_key?(temp))
-			date_map[temp]=0
-		end
-     		date_map[temp]=date_map[temp]+1
-	end
-		date_map.keys.each do |key_val|
-		     puts key_val.name
-		    puts date_map[key_val] 
-		end
+  opts.banner = "Usage: datenight.rb [options] ideas-file"
 
-#end
+  options[:weather] = 5
+  opts.on( '-w', '--weather [0-9]', 'Current weather score.' ) do |val|
+    options[:weather] = val
+  end
+
+  options[:time] = 5
+  opts.on( '-t', '--time [0-9]', 'Time commitment score.' ) do |val|
+    options[:time] = val
+  end
+
+  opts.on( '-l', '--list', 'List out the date-options in the file.' ) do
+    command = "list"
+  end
+  
+  opts.on( '-h', '--help', 'Display this screen.' ) do
+    puts opts
+    exit
+  end
+
+end
+
+optparse.parse!
+require ARGV[0]
+
+if command == "choose"
+  puts $dates.choose(options[:weather], options[:time])
+elsif command == "list"
+  puts $dates
+end
